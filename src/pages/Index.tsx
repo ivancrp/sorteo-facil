5e8df78134021ddc97bb9d91e2cc25ff41552380
Ivan Alves <49925875+ivancrp@users.noreply.github.com>
2025-03-13 15:01:24 -0300

    cor carde roleta

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -44,7 +44,7 @@ const Index = () => {
       description: 'Realize sorteios com uma roleta visual e personalizada.',
       href: '/sorteio/roleta',
       icon: Circle,
-      color: 'accent'
+      color: '#ffff7a'
     },
     {
       title: 'Sorteio de Comentários do Instagram',
